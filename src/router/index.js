5bf8fdb46f6ea4a87f4dbde04e1bcb18f8852a59
Juan Acosta <j.acosta@edu.pucrs.br>
2022-08-31 12:10:39 -0300

    views e rotas
    só não foi a pasta node_modules

---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/equipamentos',
+      name: 'equipamentos',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/EquipamentosView.vue')
+    },
+    {
+      path: '/funcionarios',
+      name: 'funcionarios',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/FuncionariosView.vue')
+    },
+    {
+      path: '/projetos',
+      name: 'projetos',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProjetosView.vue')
+    },
+    {
+      path: '/Relatorios',
+      name: 'relatorios',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/RelatoriosView.vue')
+    },
+    {
+      path: '/reservas',
+      name: 'reservas',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ReservasView.vue')
+    }
+  ]
+})
+
+export default router
