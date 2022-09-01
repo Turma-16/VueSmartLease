7afcba5b84a3ac5bf2ebbb6b76568c1e2fd4f997
deborahregina <deborahreginasilveira@gmail.com>
2022-09-01 20:04:49 -0300

    Lista de funcionarios e projetos
    Co-authored-by: Bernardo Bordin <bernardobordin77@gmail.com>
    Co-authored-by: GabrielTascaV <GabrielTascaV@users.noreply.github.com>
    Co-authored-by: Nicole Spier <DevNicole@users.noreply.github.com>
    Co-authored-by: Henrique Andreata <HnrqAndreata@users.noreply.github.com>
    Co-authored-by: Francine Hoffmann <francine-hoffmann@users.noreply.github.com>
    Co-authored-by: Felipe Nomura <felipemnomura@hotmail.com>
    Co-authored-by: Mayra Cademartori <mccademartori@inf.ufrgs.br>
    Co-authored-by: Breno W. de Assis <breno-wiebbelling@users.noreply.github.com>

--- src/views/ProjetosView.vue
+++ src/views/ProjetosView.vue
@@ -1,19 +1,147 @@
-<template>
-  <div class="about">
-    <h1>This is an Projetos page</h1>
-  </div>
-</template>
+<style scoped>
+  table{
+    background-color: rgb(254, 246, 231);
+    color: black;
+    border-collapse: collapse;
+    border-radius: 5px;
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
+  }
+  
+  thead{
+    background-color: rgb(105, 172, 156);
+    color: white;
+    font-weight: bold;
+  }
+
+  td,tr{
+    text-align: center;
+    padding: 5px 0px;
+  }
+
+  table button{
+    margin: 0px 5px;
+    cursor: pointer;
+  }
+
+  input{
+    height: 40px;
+    font-size: 15px;
+    text-indent: 10px;
+    outline: none;
+
+    border: 1px solid black;
+    border-radius: 2px;
+    letter-spacing: 1px;
+
+    margin: 10px;
+  }
+
+  input:focus{
+    background-color: rgb(243, 240, 252);
+  }
+
+  textarea{
+    width: 100%;
+    height: 150px;
+    resize: none;
+
+    font-size: 15px;
+    padding: 12px 20px;
+
+    border: 1px solid black;
+    border-radius: 4px;
+    outline: none;
+  }
+
+  textarea:focus{
+    background-color: rgb(243, 240, 252);
+  }
+
+  .button-cadastrar{
+    width: 130px;
+    height: 40px;
+    margin:10px;
+
     display: flex;
     align-items: center;
+    justify-content: center;
+
+    background-color: rgb(66, 66, 182);
+    color: white;
+    border: none;
+    border-radius: 5px;
+
+    cursor: pointer;
   }
-}
+
+  .button-cadastrar:hover{
+    background-color: rgb(88, 88, 166);
+
+  }
+
+  .button-cadastrar i{
+    margin-left: 10px;
+    color:white;
+  }
+
+  .formCadastro{
+    width: 50vw;
+    height: 400px;
+
+    margin: 30px 0px;
+
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    background-color: rgb(254, 246, 231);
+    color: black;
+    border-radius: 5px;
+
+    display: none;
+  }
+
+  .formSelected{
+    display:flex;
+  }
+
 </style>
 
-<script setup>
+<template >
+  <h1>Lista de Projetos</h1>
+ 
+  <table>
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Nome</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody v-for="p in listaDeProjetos.projeto">  
+      <tr>
+        <td>{{p.projetoId}}</td>
+        <td>{{p.nome}}</td>
 
+      </tr>
+    </tbody>
+  </table>
+
+    
+</template>
+
+<script setup>
+  import {onMounted, onUpdated, reactive, ref} from "vue";
+  import https from "../services/https.js";
+  import 'primeicons/primeicons.css';
+
+  let listaDeProjetos = reactive({projeto:[]});
+
+
+  onMounted(async () =>{
+    var {data} = await https.get('Projeto/Listar');
+    listaDeProjetos.projeto = data;
+  })
+
+  
 </script>
